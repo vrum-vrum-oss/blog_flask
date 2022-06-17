217e3699e97708c0bbe74c303780c49157af9413
cawakowa <kovalalkonst@gmail.com>
2022-06-17 16:51:24 +0300

    Implement index page functionality in a blueprint
    Define main blueprint, register it with the app.
    Associate route '/' and index() view function with main blueprint
    Create index template

--- blog.py
+++ blog.py
@@ -1,6 +1,7 @@
 import os
 from app import create_app
 
-if __name__ == 'main':
+
+if __name__ == '__main__':
     app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-    app.run() 
+    app.run()
