da3d8fc66752bf468d91eb3ce6d07d502a756242
cawakowa <kovalalkonst@gmail.com>
2022-06-26 02:41:23 +0300

    Add many-to-many Post-Tag relationship

--- app/models.py
+++ app/models.py
@@ -9,12 +9,21 @@ def slugify(s):
     return re.sub('_$','', temp)
 
 
+post_tags = db.Table(
+    'post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id')),
+)
+
+
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(140))
     slug = db.Column(db.String(140), unique=True)
     body = db.Column(db.Text)
     created = db.Column(db.DateTime, default=datetime.now())
+
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
 
 
     def __init__(self, *args, **kwargs):
