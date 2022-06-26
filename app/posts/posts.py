b2b6d33b8e47b645fbd3b2b43eb6b2b6de7e8f4a
cawakowa <kovalalkonst@gmail.com>
2022-06-27 02:34:11 +0300

    Add tags to posts and a view with posts related to a tag

--- app/posts/posts.py
+++ app/posts/posts.py
@@ -1,17 +1,24 @@
 from . import posts_bp
 from flask import render_template
 from .. import db
-from ..models import Post
+from ..models import Post, Tag
 
 
 @posts_bp.route('/')
 def view():
     posts = Post.query.all()
-
     return render_template('posts/blog_view.html', posts=posts)
 
 
 @posts_bp.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter_by(slug=slug).first()
-    return render_template('posts/post_detail.html', post=post)
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
+
+
+@posts_bp.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter_by(slug=slug).first()
+    posts = tag.posts.all()
+    return render_template('posts/tag_detail.html', tag=tag, posts=posts)
